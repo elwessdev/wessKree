eb0b14475675ae6ea996397e4b7af156e81e2d3d
elwessdev <benaliosama3@gmail.com>
2025-01-15 00:02:04 +0100

    Home page

---
+++ src/Header/header.tsx
@@ -0,0 +1,13 @@
+
+import "./header.scss"
+
+export default function Header(){
+    return (
+        <div id="header">
+            <div className="content">
+                <h1>Find your fresh start</h1>
+                <h3>Discover millions of houses and apartments for rent.</h3>
+            </div>
+        </div>
+    )
+}
---
+++ src/Nav/nav.tsx
@@ -0,0 +1,83 @@
+import "./nav.scss"
+import { TbHomeSearch } from "react-icons/tb";
+import { Select } from 'antd';
+
+const options = [
+    {
+        value: '1',
+        label: 'Not Identified',
+    },
+    {
+        value: '2',
+        label: 'Closed',
+    },
+    {
+        value: '3',
+        label: 'Communicated',
+    },
+    {
+        value: '4',
+        label: 'Identified',
+    },
+    {
+        value: '5',
+        label: 'Resolved',
+    },
+    {
+        value: '6',
+        label: 'Cancelled',
+    },
+]
+
+export default function Nav(){
+    return(
+        <div id="nav" className="container mx-auto">
+            <div className="l-s">
+                <div className="logo">
+                    KreeTN
+                </div>
+            </div>
+            <div className="c-s">
+                <div className="search-bar">
+                    <div className="slc">
+                        <div className="sl">
+                            <span className="subTitle">State</span>
+                            <Select
+                                showSearch
+                                style={{ width: "100%" }}
+                                placeholder="Search..."
+                                optionFilterProp="label"
+                                filterSort={(optionA, optionB) =>
+                                (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
+                                }
+                                options={options}
+                            />
+                        </div>
+                        <div className="sl">
+                            <span className="subTitle">City</span>
+                            <Select
+                                showSearch
+                                style={{ width: "100%" }}
+                                placeholder="Search..."
+                                optionFilterProp="label"
+                                filterSort={(optionA, optionB) =>
+                                (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
+                                }
+                                options={options}
+                            />
+                        </div>
+                    </div>
+                    <button>
+                        <TbHomeSearch />
+                    </button>
+                </div>
+            </div>
+            <div className="r-s">
+                <div className="btns">
+                    <a>Login</a>
+                    <a>Sign up</a>
+                </div>
+            </div>
+        </div>
+    )
+}
