11ae135ca4573cf52e707d6445609ece80056922
elwessdev <benaliosama3@gmail.com>
2025-01-17 19:14:10 +0100

    UI - Auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 // import Logo from '/logo.jpeg'
 import { Outlet } from "react-router-dom";
 import Nav from "./Nav/nav";
-import Footer from "./Footer/footer";
+// import Footer from "./Footer/footer";
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Outlet />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   )
 }
