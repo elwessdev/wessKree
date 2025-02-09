a0e86718d0676e4d771fc7d4118a3d77dfe8f250
elwessdev <benaliosama3@gmail.com>
2025-02-10 00:53:18 +0100

    FIX - Server CORS

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), 'VITE_');
+  console.log("Proxy Target:", env.VITE_API_URL);
   return {
     plugins: [react()],
     server: {
